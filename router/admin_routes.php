4450f9b05bbd1efd181e89e3828c52c10c33fd58
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-04 18:19:45 +0100

    hotfix in index getting controller

--- router/admin_routes.php
+++ router/admin_routes.php
@@ -1,5 +1,5 @@
 <?php
 	//contiendra les test pour savoir si on doit inclure un controller getter
 	if ($page == "index") {
-		require_once(ADMCTRROOT."index.php");
+		require_once(ADMCTRROOT."initialise/index.php");
 	}
