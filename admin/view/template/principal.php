985d020800af5c6948d1e6620650aa1fdb4af8df
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-04 15:25:45 +0100

    add admin

---
+++ admin/view/template/principal.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ChmapiSpot admin</title>
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+		
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?=TPLWEBROOT?>css/style.css">
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<!-- ce fichier incluera les pages des vues -->
+		<!-- header goes here -->
+		
+		<?php require_once(ROOT."admin/view/".$page.".php") ?>
+		
+		<!-- footer goes here -->
+	</body>
+</html>
