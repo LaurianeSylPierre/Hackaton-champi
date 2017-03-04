a1018bb149432f90d259ea0ccf3c7c63b25567c6
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-04 12:11:52 +0100

    add router base system

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 	/** router */
+	
+	if ((isset($_GET['page'])) && ($_GET['page'] != "")) {
+		$page = $_GET['page'];
+	}
+	else {
+		$page = "index.php";
+	}
+	
+	require_once("app/view/template/principal.php");
