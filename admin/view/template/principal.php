9d1dfc4906a9e37c4db50e0b7d2b7a865d0272b5
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-04 18:06:28 +0100

    add jQuery

--- admin/view/template/principal.php
+++ admin/view/template/principal.php
@@ -8,6 +8,7 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?=TPLWEBROOT?>css/style.css">
 		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
