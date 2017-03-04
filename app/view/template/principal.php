6832bbc85dbe8e14c1cdcbb24dac31ac905bb121
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-04 14:54:05 +0100

    add bootstrap and style css in head of template

--- app/view/template/principal.php
+++ app/view/template/principal.php
@@ -1,7 +1,14 @@
 <!doctype html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<title>ChmapiSpot</title>
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
 		
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?=TPLWEBROOT?>css/style.css">
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<!-- ce fichier incluera les pages des vues -->
