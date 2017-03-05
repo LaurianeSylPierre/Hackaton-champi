d7093e9e7a1c58cf00c5236cb4dbf409d3c888a4
Unknown <laurianepierre@outlook.fr>
2017-03-05 14:47:10 +0100

    files puts in the good folder

--- bdd.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("DB_TYPE", "mysql");
-	define("DB_NAME", "appli-champi");
-	define("DB_USER", "appli-champi");
-	define("DB_PASS", "pDgMtgMoPIOgY9QW");
-	define("DB_HOST", "127.0.0.1");
