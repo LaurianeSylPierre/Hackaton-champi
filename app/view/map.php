e6b7508e11ff74fa140ec71d21da746ba94e5a25
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-05 15:33:17 +0100

    add like or dislike in ajax

--- app/view/map.php
+++ app/view/map.php
@@ -21,8 +21,8 @@
 					(champignon.toxique[i] == null)?'<img style="height: 33px; float: right;" src="<?=TPLWEBROOT?>img/icone_mini_skull.svg">'+
 					champignon.nom+'<br>'+
 					champignon.posx[i]+', '+champignon.posy[i]+
-					'<a><img style="height: 33px;" src="<?=TPLWEBROOT?>img/icone_mini_thumb_like.svg"></a>'+
-					'<a><img style="height: 33px;" src="<?=TPLWEBROOT?>img/icone_mini_thumb_dislike.svg"></a>':
+					'<a><img class="like" type="like" id-champ="'+champignon.id_champignon[i]+'" style="height: 33px;" src="<?=TPLWEBROOT?>img/icone_mini_thumb_like.svg"></a>'+
+					'<a><img class="like" type="dislike" id-champ="'+champignon.id_champignon[i]+'" style="height: 33px;" src="<?=TPLWEBROOT?>img/icone_mini_thumb_dislike.svg"></a>':
 					champignon.nom+'<br>'+
 					champignon.posx[i]+', '+champignon.posy[i]);
 		}
@@ -34,3 +34,32 @@
 		 markers.addLayer(L.marker([47.2374, 6.024]));
 		 mymap.addLayer(markers);*/
 		</script>
+		   
+		   
+		    <script>
+			    $(document).ready(function() {
+			        $(document).on("click", ".like", function(e) {
+                        e.preventDefault();
+                        console.log("dgdgdfg");
+                        
+                        var type = $(this).attr("type");
+                        var id_champ = $(this).attr("id-champ");
+                        
+                        if (type == "like") {
+                            var url = "<?=WEBROOT?>controller/note/like";
+                        }
+                        else {
+                            var url = "<?=WEBROOT?>controller/note/dislike";
+                        }
+                
+                        $.ajax({
+                            type:"GET",
+                            url:url,
+	                        data:"id_champignon="+id_champ,
+                            success: function(data){
+                                $("body").prepend(data);
+                            }
+                        });
+			        })
+			    })
+		    </script>
