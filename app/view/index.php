da580d99a51467c168166768f3ba738e2a5c380b
Unknown <manu_guillou@yahoo.fr>
2017-03-05 16:56:19 +0100

    add index view

--- app/view/index.php
+++ app/view/index.php
@@ -1,1 +1,45 @@
 <!-- page d'accueil du site inclue dans template/principal.php -->
+	<!-- ce fichier incluera les pages des vues -->
+		<!-- header goes here -->
+		<header>
+		<div id="header">
+			<div class="container">
+			
+				<div  class="row">
+					<div class="col-xs-6 col-xs-offset-1">
+						<img src="<?=TPLWEBROOT?>img/cs_logo1.svg"/>
+					</div>
+					
+					<div id="more_spot" class="col-xs-2 col-xs-offset-3">
+						<img class="icone" id="cross" src="<?=TPLWEBROOT?>img/button_whitecross.svg"/>
+					</div>
+				</div>
+			</div>
+			</div>
+		</header>
+		
+		
+		
+		<!-- footer goes here -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-9 col-xs-offset-1">
+						<img src="<?=TPLWEBROOT?>img/button_loupe.svg"/>
+						<form class="searchbar">
+							<select>
+								<option selected> Choisissez votre type de champignons</option>
+								<option value="morilles">Morilles</option>
+								<option value="amanites">Amanites </option>
+								<option value="cepes">Cèpes</option>
+							</select>
+						</form>
+					
+						
+					</div>
+					<div id="more_spot" class="col-xs-2">
+						<img class="icone" src="<?=TPLWEBROOT?>img/button_info.svg"/>
+					</div>
+				</div>
+			</div>
+		</footer>
