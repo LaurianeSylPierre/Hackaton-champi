5df12386a1eddbcd05275a2e9eb360102c3fd747
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-05 17:17:45 +0100

    add 404 style

--- app/view/carapace-rouge.php
+++ app/view/carapace-rouge.php
@@ -1,1 +1,8 @@
-<h1>Carapace rouge</h1>
+<div class="container" style="margin-top: 150px;">
+	<div class="row">
+		<div class="col-xs-12 text-center">
+			<h1>Carapace rouge</h1>
+			<p>La page demandée n'existe pas !</p>
+		</div>
+	</div>
+</div>
